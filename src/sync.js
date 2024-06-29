3cb40d72ebbd664823501be218c176b60fb7136f
Mark <mark@humanscape.io>
2024-06-29 10:02:40 +0900

    feat: sync 기능 추가

--- src/sync.js
+++ src/sync.js
@@ -1,5 +1,7 @@
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager'
+import { ScanCommand, DynamoDBClient } from '@aws-sdk/client-dynamodb'
 
+import dayjs from 'dayjs'
 import inquirer from 'inquirer'
 import fs from 'fs'
 import path from 'path'
@@ -23,23 +25,62 @@ const sync = async () => {
   console.log('동기화를 시작합니다.')
   const envData = JSON.parse(fs.readFileSync(syncEnvFilePath, 'utf8'))
 
-  for (const { key, path, region } of envData) {
-    console.log(`Syncing ${key} to ${path}`)
-    const secrets = await getSecretValues(region, key)
-    const envContent = Object.entries(secrets)
-      .map(([key, value]) => `${key}=${value}`)
-      .join('\n')
-    fs.writeFileSync(path, envContent)
+  console.log('동기화 정보를 가져옵니다.')
+  const syncInfo = await getSyncInfo()
+
+  for (const { key, path, region, syncedAt } of envData) {
+    console.log(`${key}의 동기화 정보를 확인합니다.`)
+
+    const syncItem = syncInfo.find(item => item.SecretName.S === key)
+
+    if (!syncItem) {
+      console.log(`Secret ${key} 정보가 없습니다. DynamoDB에 추가해주세요.`)
+      continue
+    }
+
+    if (!syncedAt || dayjs(syncItem.LastChangedDate.S) >= dayjs(syncedAt)) {
+      console.log(`Secret ${key}가 업데이트 되었으므로 동기화를 진행합니다.`)
+
+      await saveSecretValues(region, key, path)
+      await updateSyncedAt(key)
+    } else {
+      console.log(`Secret ${key}가 최신 상태입니다. 동기화를 진행하지 않습니다.`)
+    }
   }
 
   console.log('동기화 완료!')
 }
 
-async function getSecretValues (region, SecretId) {
+async function updateSyncedAt (key) {
+  const existingConfigs = JSON.parse(
+    fs.readFileSync(syncEnvFilePath, 'utf8')
+  )
+  const existingConfig = existingConfigs.find(
+    (config) => config.key === key
+  )
+  existingConfig.syncedAt = dayjs().toISOString()
+  fs.writeFileSync(syncEnvFilePath, JSON.stringify(existingConfigs))
+}
+
+async function saveSecretValues (region, SecretId, envPath) {
   const client = new SecretsManagerClient({ region })
   const command = new GetSecretValueCommand({ SecretId })
   const { SecretString } = await client.send(command)
-  return JSON.parse(SecretString)
+  const envContent = Object.entries(JSON.parse(SecretString))
+    .map(([key, value]) => `${key}=${value}`)
+
+    .join('\n')
+  fs.writeFileSync(envPath, envContent)
+}
+
+async function getSyncInfo () {
+  const client = new DynamoDBClient({ region: 'ap-northeast-2' })
+  const command = new ScanCommand({
+    TableName: 'syncenv'
+  })
+
+  const { Items } = await client.send(command)
+  return Items
 }
 
 export default sync
